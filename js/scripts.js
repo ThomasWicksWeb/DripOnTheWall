33c086925b91815abf7c4f187e89a1420cbdc24a
ThomasWicksWeb <52025129+ThomasWicksWeb@users.noreply.github.com>
2019-12-04 21:17:17 -0500

    Initial Upload
    Work-in-progress initial pload

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    // NAVBAR
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function () {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+
+
+    // Removing bouncing arrow on scroll
+    let arrow = $('.arrowBounce');
+    function fixArrow() {
+        if (window.scrollY >= 150) {
+            arrow.fadeOut(450);
+        } else {
+            arrow.fadeIn(450);
+        }
+    };
+    window.addEventListener('scroll', fixArrow);
+
+
+    // Slick.js Thing
+    $('.slickParent').slick({
+        
+      });
+
+});
